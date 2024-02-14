c4c3ebf2818ec6e3efee3fd90240f71d49900590
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 12:25:00 -0800

    Add files via upload
    Q1

---
+++ Q1.ts
@@ -0,0 +1,7 @@
+
+// Q1.Install Node.js, TypeScript and VS Code on your computer.
+
+1.// https://code.visualstudio.com/download for (vs code)
+2. //nodejs.org/en/download for (nodejs)
+3.// npm i -g typescript  for (typescript)
+4. //tsc -- init  for (tsconfig.json)
